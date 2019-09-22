519c9aa3d8b534def76f794f1fba2716874215bf
gavra96 <gavrilov.nikola96@gmail.com>
2019-09-22 15:28:08 +0200

    small update

---
+++ movies/src/Components/Nav/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const nav = props => {
+
+    return (
+        <div>
+        <header id="header" className="alt">
+            <a href="index.html" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></a>
+            <nav>
+            <a href="#menu">Menu</a>
+            </nav>
+        </header>
+
+
+        <nav id="menu">
+            <ul className="link">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="landing.html">Landing</a></li>
+            <li><a href="generic.html">Generic</a></li>
+            <li><a href="elements.html">Elements</a></li>
+            </ul>
+            <ul className="actions stacked">
+            <li><a href="#" className="button primary fit">Get Started</a></li>
+            <li><a href="#" className="button fit">Log In</a></li>
+            </ul>
+        </nav>
+      </div>
+    );
+
+};
+
+export default nav;
