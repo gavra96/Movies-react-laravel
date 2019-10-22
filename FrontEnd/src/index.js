84f7ef81bb73b3ea5ff6dfc9681ceac54876098f
gavra96 <gavrilov.nikola96@gmail.com>
2019-10-22 22:22:50 +0200

    Almost finished auth
     to be continiued

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,10 +4,26 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+
+import authReducer from './store/reducers/auth';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(authReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+ // redux not tested yet
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
--- FrontEnd/src/store/actions/auth.js
+++ FrontEnd/src/store/actions/auth.js
@@ -1,1 +1,58 @@
-// TO DO
+import * as actionTypes from './actionTypes';
+import axios from 'axios'; 
+// TO DO setup default axios instance 
+// header setup for axios if user is loged in 
+//{
+  //  headers: {
+    //  Authorization: 'Bearer ' + myToken
+    //}
+ // }
+
+export const authStart = () => {
+    return {
+        type : actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type : actionTypes.AUTH_SUCCESS,
+        authData : authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type : actionTypes.AUTH_FAIL,
+        error : error
+    };
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    return {
+        type : actionTypes.AUTH_LOGOUT
+    };
+};
+
+// TO DO auto loging
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart);
+        const authData = {
+            email,
+            password
+        };
+        axios.post('localhost:8001/api/login', authData, {
+            Accept : 'application/json'
+        }).then(response => {
+            console.log(response);
+            //localStorage.setItem('token', response.data.token); 
+            //dispatch(authSuccess(response.data.token, response.data.user)); check from console log first
+        }).catch(error => {
+            console.log(error);
+            //dispatch(authFail(error.response.message)); console log first
+        });
+    };
+};
